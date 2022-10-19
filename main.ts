ab701ff2aca032a8685b9754239b69d41f18ec06
AdrianoGutierrez <37429722+AdrianoGutierrez@users.noreply.github.com>
2022-10-18 23:03:22 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,78 @@
+basic.forever(function () {
+    basic.showString("U1")
+    basic.clearScreen()
+    basic.pause(2000)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . . .
+        # # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(4000)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+})
